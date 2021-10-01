9b5930774ac33c57ed86558a07df9e2ebba272a8
silverd <silverd29@gmail.com>
2021-10-02 07:45:32 +0800

    -

--- sh/updated.sh
+++ sh/updated.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+mkdir -m 777 -p bootstrap/cache
+mkdir -m 777 -p storage/logs
+mkdir -m 777 -p storage/app/public
+mkdir -m 777 -p storage/framework/cache
+mkdir -m 777 -p storage/framework/views
+mkdir -m 777 -p storage/framework/sessions
+
 chmod -R 777 storage
 chmod -R 777 bootstrap/cache
-chmod -R 777 /tmp/symfony-cache
+
+if [ -d '/tmp/symfony-cache' ]; then
+  chmod -R 777 /tmp/symfony-cache
+fi
+
