bd4dbfb2a260a11d8919cc9079e9bf0f81a2fb88
zed.zhao <954489346zyz@sina.com>
2024-01-26 18:16:42 +0800

    fix: 错误修复

--- src/Services/TableauService.php
+++ src/Services/TableauService.php
@@ -190,7 +190,7 @@ class TableauService extends AbstractService
     }
 
     // @see https://help.tableau.com/current/api/rest_api/en-us/REST/rest_api_ref_users_and_groups.htm#remove_user_from_site
-    public function removeUserFromSite(string $mapAssetsToUserId)
+    public function removeUserFromSite(string $mapAssetsToUserId, int $userId)
     {
         $params = [
             'mapAssetsTo' => $mapAssetsToUserId,
@@ -374,7 +374,7 @@ class TableauService extends AbstractService
         return $this->requestWithToken('DELETE', '/sites/' . $this->config['site_id'] . '/subscriptions/' . $subscriptionId);
     }
 
-    public function updateSubscription(string $subscriptionId)
+    public function updateSubscription(string $subscriptionId, array $params)
     {
         $updated = $this->getParams($params, [
             'subject',
@@ -444,7 +444,7 @@ class TableauService extends AbstractService
         });
     }
 
-    public function getWorkbooksByNames(array $workbookNames)
+    public function getWorkbooksByNames(array $workbookNames, array $schedules)
     {
         $workbooks = $this->queryWorkbooks();
 
