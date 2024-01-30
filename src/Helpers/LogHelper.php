7344434a421ed6cecea1753a1892bf864ea1321d
silverd <silverd29@gmail.com>
2024-01-30 18:11:08 +0800

    -

---
+++ src/Helpers/LogHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Silverd\OhMyLaravel\Helpers;
+
+use Monolog\Logger;
+use Monolog\Handler\MongoDBHandler;
+use Monolog\Formatter\MongoDBFormatter;
+use Silverd\OhMyLaravel\Extensions\Logger\Handler\DatabaseHandler;
+
+class LogHelper
+{
+    public static function makeMySQLDbChannel(array $channels)
+    {
+        $return = [];
+
+        foreach ($channels as $channel) {
+            $return[$channel] = [
+                'driver'  => 'monolog',
+                'handler' => DatabaseHandler::class,
+                'with'    => [
+                    'level'      => Logger::DEBUG,
+                    'table'      => 'log_' . $channel,
+                    'rotate'     => 'ymd',
+                    'connection' => 'mysql_log',
+                ],
+            ];
+        }
+
+        return $return;
+    }
+
+    public static function makeMongoDbChannel(array $channels)
+    {
+        $return = [];
+
+        foreach ($channels as $channel) {
+            $return[$channel] = [
+                'driver'    => 'monolog_log',
+                'handler'   => MongoDBHandler::class,
+                'formatter' => MongoDBFormatter::class,
+                'with'      => [
+                    'level'      => Logger::DEBUG,
+                    'database'   => env('DB_LOG_DATABASE'),
+                    'collection' => 'log_' . $channel,
+                    'connection' => 'mongodb_log',
+                ],
+                'formatter_with' => [
+                    'maxNestingLevel' => 10,
+                ],
+            ];
+        }
+
+        return $return;
+    }
+}
