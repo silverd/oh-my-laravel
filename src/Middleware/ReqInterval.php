4aa831749f31596749942de9dca3a0028c0c2cbb
silverd <silverd29@gmail.com>
2024-04-04 02:22:23 +0800

    -

---
+++ src/Middleware/ReqInterval.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Silverd\OhMyLaravel\Middleware;
+
+use Auth;
+use Cache;
+use Closure;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use RuntimeException;
+
+class ReqInterval
+{
+    public function handle(Request $request, Closure $next, int $seconds = 1, string $namespace = null)
+    {
+        $cacheKey = self::getUniqueKey($request, $namespace);
+
+        if ($diffSecs = self::calcNextReqDiffSecs($cacheKey, $seconds)) {
+            throws('操作频繁，请' . $diffSecs . '秒后重试', -2);
+        }
+
+        return $next($request);
+    }
+
+    // 返回下次请求仍需等待的秒数
+    private static function calcNextReqDiffSecs(string $cacheKey, int $seconds = 1)
+    {
+        $currentTime = time();
+
+        if (Cache::add($cacheKey, $currentTime, $seconds)) {
+            return 0;
+        }
+
+        $lastReqTime = Cache::get($cacheKey);
+
+        // 两次请求间隔不得少于N毫秒
+        if ($lastReqTime && ($diffSecs = $lastReqTime + $seconds - $currentTime) > 0) {
+            return $diffSecs;
+        }
+
+        return 0;
+    }
+
+    // 构造请求标识
+    private static function getUniqueKey(Request $request, string $namespace = null)
+    {
+        if ($user = $request->user()) {
+            $uniqueKey = sha1($user->getAuthIdentifier());
+        }
+        elseif ($route = $request->route()) {
+            $uniqueKey = sha1($route->getDomain() . '|' . $request->ip());
+        }
+        else {
+            throw new RuntimeException('Unable to generate the request signature. Route unavailable.');
+        }
+
+        $routeStr = sha1(strtolower($request->method() . ':' . $request->path()));
+
+        return 'LastReqTime:' . $uniqueKey . ':' . $routeStr . ($namespace ? (':' . $namespace) : '');
+    }
+}
